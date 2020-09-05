205849bb6ab2b215ea277cccf419a276c1aecc75
jucchann <kanakosuzuki.315@gmail.com>
2020-09-05 17:20:48 +0900

    マイページ、編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
 
   def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
