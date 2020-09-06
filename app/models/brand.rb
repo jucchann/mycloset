283db8771a51c8f05032b06a71391f06b48baf15
jucchann <kanakosuzuki.315@gmail.com>
2020-09-06 16:55:20 +0900

    ブランド登録

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,34 @@
 class Brand < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'ユニクロ' }
+    { id: 2, name: 'agnes b' },
+    { id: 3, name: 'AMERICAN RAG CIE' },
+    { id: 4, name: 'A.P.C' },
+    { id: 5, name: 'beautiful people' },
+    { id: 6, name: 'BIRKENSTOCK' },
+    { id: 7, name: 'CANADA GOOSE' },
+    { id: 8, name: 'Champion' },
+    { id: 9, name: 'CLANE' },
+    { id: 10, name: 'CONVERSE TOKYO' },
+    { id: 11, name: 'DANTON' },
+    { id: 12, name: 'FALKE' },
+    { id: 13, name: 'gelato pique' },
+    { id: 14, name: 'GU' },
+    { id: 15, name: 'IENA' },
+    { id: 16, name: 'JOURNAL STANDARD' },
+    { id: 17, name: 'Kastane' },
+    { id: 18, name: 'Laguna Moon' },
+    { id: 19, name: 'Lily Brown' },
+    { id: 20, name: 'Masion Kitsune' },
+    { id: 21, name: 'MHL' },
+    { id: 22, name: 'MILKFED' },
+    { id: 23, name: 'NANO UNIVERSE' },
+    { id: 24, name: 'NIKE' },
+    { id: 25, name: 'PLST' },
+    { id: 26, name: 'ユニクロ' },
+    { id: 27, name: 'UNITED ARROWS' },
+    { id: 28, name: 'URBAN RESEARCH' },
+    { id: 29, name: 'Y-3' },
+    { id: 30, name: 'ZARA' }
   ]
 end
